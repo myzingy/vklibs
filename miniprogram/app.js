080fa75070a8c526f3dd9c4a72688a87a04c4684
vking <251941666@qq.com>
2019-06-20 15:57:32 +0800

    first

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,35 +1,22 @@
 //app.js
+import {vk,regeneratorRuntime,PubSub} from 'vktool'
+import request from 'request/index'
 App({
+  ...vk,
+  ...request,
+  PubSub:PubSub,
+  regeneratorRuntime:regeneratorRuntime,
+  globalData: {
+    userInfo: null
+  },
   onLaunch: function() {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    this.config({
+      request:{
+        responseKey:'Response', //Response 则使用网络请求状态判断，其它值则使用res.StatusKey 进行判断
+        responseCode:200,   //正常返回结果 StatusKey的值 == StatusCode 视为正常结果
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+        responseKeyData:'content',   //错误信息的key
+        responseKeyMsg:'msg',   //错误信息的key
       }
     })
     // 获取系统状态栏信息
@@ -42,7 +29,7 @@ App({
       }
     })
   },
-  globalData: {
-    userInfo: null
+  onHide(){
+    this.cache_clear()
   }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const {regeneratorRuntime} = app
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
